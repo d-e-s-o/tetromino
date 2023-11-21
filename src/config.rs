669f5541eb562f8cb125fb88472934313c4dbf66
Daniel Müller <deso@posteo.net>
2023-11-20 21:47:01 -0800

    Provide proper defaults for game configuration attributes
    The way our Config types are defined, serde is only able to deserialize
    a complete object with all fields set. That is obviously not great once
    we start adding new members, because a mere addition then causes a
    deserialization failure of a previously-valid configuration.
    With this change we annotate individual attributes with default values
    that serde can use, resulting in a much more flexible arrangement: any
    value left unspecified by the user in the configuration file will just
    be populated with a default. We do that for the overall config and the
    game specific part. For the keyboard configuration we preserve
    all-or-nothing semantics, to not clutter the code overly much (due to
    API constraints, we always query both attributes in tandem).

--- src/config.rs
+++ src/config.rs
@@ -11,7 +11,46 @@ use crate::keys;
 #[derive(Default, Debug, Deserialize, Serialize)]
 pub struct Config {
   /// Settings pertaining keyboard handling.
+  #[serde(default)]
   pub keyboard: keys::Config,
   /// Configuration of the game itself.
+  #[serde(default)]
   pub game: game::Config,
+}
+
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+
+  /// Make sure that we can successfully deserialize various
+  /// configurations.
+  #[test]
+  fn deserialization() {
+    // Complete config with everything set.
+    let config = Config::default();
+    let config = toml::to_string_pretty(&config).unwrap();
+    assert!(toml::from_str::<Config>(&config).is_ok());
+
+    // Config without keyboard data.
+    let config = r#"
+[game]
+start_level = 1
+lines_for_level = 10
+field_width = 10
+field_height = 20
+preview_stone_count = 1
+enable_ai = false
+    "#;
+    assert!(toml::from_str::<Config>(config).is_ok());
+
+    // Partial game config.
+    let config = r#"
+[game]
+start_level = 1
+lines_for_level = 10
+    "#;
+    assert!(toml::from_str::<Config>(config).is_ok());
+  }
 }
--- src/game/config.rs
+++ src/game/config.rs
@@ -5,31 +5,58 @@ use serde::Deserialize;
 use serde::Serialize;
 
 
+fn default_start_level() -> u16 {
+  1
+}
+
+fn default_lines_for_level() -> u16 {
+  10
+}
+
+fn default_field_width() -> i16 {
+  10
+}
+
+fn default_field_height() -> i16 {
+  20
+}
+
+fn default_preview_stone_count() -> u8 {
+  1
+}
+
+
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Config {
   /// The level the game starts at.
+  #[serde(default = "default_start_level")]
   pub start_level: u16,
   /// The number of lines required to level up.
+  #[serde(default = "default_lines_for_level")]
   pub lines_for_level: u16,
   /// The width of the tetris field.
+  #[serde(default = "default_field_width")]
   pub field_width: i16,
   /// The height of the tetris field.
+  #[serde(default = "default_field_height")]
   pub field_height: i16,
   /// The number of upcoming stones to show.
+  #[serde(default = "default_preview_stone_count")]
   pub preview_stone_count: u8,
   /// Whether or not the AI is enabled initially.
+  #[serde(default)]
   pub enable_ai: bool,
 }
 
 impl Default for Config {
   fn default() -> Self {
     Self {
-      start_level: 1,
-      lines_for_level: 10,
-      field_width: 10,
-      field_height: 20,
-      preview_stone_count: 1,
-      enable_ai: false,
+      start_level: default_start_level(),
+      lines_for_level: default_lines_for_level(),
+      field_width: default_field_width(),
+      field_height: default_field_height(),
+      preview_stone_count: default_preview_stone_count(),
+      enable_ai: Default::default(),
     }
   }
 }
