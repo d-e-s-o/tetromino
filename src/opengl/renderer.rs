a517f6d0981c0a789dd40fd3ebaa749c508febb8
Daniel Müller <deso@posteo.net>
2024-07-30 22:50:44 -0700

    Adjust Renderer::on_pre_render() documentation
    The documentation of the Renderer::on_pre_render() method still talks
    about a window, but since commit 9ddf666629da ("Adjust
    Renderer::on_pre_render() to require Context object") it actually
    accepts a Context.
    Adjust the doc comment accordingly.

--- src/opengl/renderer.rs
+++ src/opengl/renderer.rs
@@ -666,10 +666,10 @@ impl Renderer {
     }
   }
 
-  /// Activate the renderer with the given [`Window`] in preparation for
-  /// rendering to take place.
-  // This method requires an exclusive `Window` reference do ensure that
-  // while a renderer is active the window can't swap buffers, for
+  /// Activate the renderer with the given [`Context`] in preparation
+  /// for rendering to take place.
+  // This method requires an exclusive `Context` reference do ensure
+  // that while a renderer is active the context can't swap buffers, for
   // example.
   pub fn on_pre_render<'ctx>(&'ctx self, context: &'ctx mut Context) -> ActiveRenderer<'ctx> {
     let _ = context;
