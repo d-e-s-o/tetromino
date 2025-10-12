868de4b0e686c27e529d964c30ef91486d60d3c8
Daniel Müller <deso@posteo.net>
2025-10-12 08:18:40 -0700

    Use runtime environment variable query in build script
    It is incorrect to assume presence of the various environment variables
    used in the build script snippet at compile time [0]. Switch to using
    runtime querying instead.
    
    [0] https://doc.rust-lang.org/cargo/reference/environment-variables.html#environment-variables-cargo-sets-for-build-scripts

--- build.rs
+++ build.rs
@@ -3,6 +3,8 @@
 
 //! Build script for `tetromino`.
 
+use std::env;
+
 use grev::git_revision_auto;
 
 // OpenGL 1.3 is guaranteed to be available on Linux, so it's fine
@@ -14,6 +16,9 @@ const OPENGL_MINOR: u8 = 3;
 fn main() {
   println!("cargo:rustc-env=OPENGL_MAJOR={OPENGL_MAJOR}");
   println!("cargo:rustc-env=OPENGL_MINOR={OPENGL_MINOR}");
+
+  let manifest_dir =
+    env::var_os("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR variable not set");
 
   #[cfg(feature = "generate-opengl-bindings")]
   {
@@ -27,8 +32,7 @@ fn main() {
     use gl_generator::Registry;
     use gl_generator::StaticGenerator;
 
-    let crate_dir = env!("CARGO_MANIFEST_DIR");
-    let dst = Path::new(&crate_dir)
+    let dst = Path::new(&manifest_dir)
       .join("src")
       .join("opengl")
       .join("bindings.rs");
@@ -45,14 +49,11 @@ fn main() {
     .unwrap();
   }
 
-  let dir = env!("CARGO_MANIFEST_DIR");
-  if let Some(git_rev) = git_revision_auto(dir).unwrap() {
-    println!(
-      "cargo:rustc-env=VERSION={} (@ {})",
-      env!("CARGO_PKG_VERSION"),
-      git_rev
-    );
+  let pkg_version = env::var("CARGO_PKG_VERSION").expect("CARGO_PKG_VERSION variable not set");
+
+  if let Some(git_rev) = git_revision_auto(manifest_dir).unwrap() {
+    println!("cargo:rustc-env=VERSION={pkg_version} ({git_rev})");
   } else {
-    println!("cargo:rustc-env=VERSION={}", env!("CARGO_PKG_VERSION"));
+    println!("cargo:rustc-env=VERSION={pkg_version}");
   }
 }
