acc1a464818d9cd4dc72889e3577904dc0e00b28
Daniel Müller <deso@posteo.net>
2026-09-06 10:37:55 -0700

    Update grev crate to 0.2
    Update the grev crate to version 0.2 to get the latest and greatest.

--- build.rs
+++ build.rs
@@ -10,7 +10,7 @@ use std::path::PathBuf;
 #[cfg(feature = "deploy")]
 use std::process::Command;
 
-use grev::git_revision_auto;
+use grev::git_revision;
 
 
 /// Generate the final WASM bindings package in `output_dir` based on
@@ -124,7 +124,8 @@ fn main() {
     env::var_os("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR variable not set");
   let pkg_version = env::var("CARGO_PKG_VERSION").expect("CARGO_PKG_VERSION variable not set");
 
-  if let Some(git_rev) = git_revision_auto(&manifest_dir).unwrap() {
+  let git_rev = git_revision(&manifest_dir).expect("failed to query Git revision");
+  if let Some(git_rev) = git_rev {
     println!("cargo:rustc-env=VERSION={pkg_version} ({git_rev})");
   } else {
     println!("cargo:rustc-env=VERSION={pkg_version}");
